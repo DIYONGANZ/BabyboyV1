2d50028882d0ffdcc35d8082c8850102c2e01d51
DIYONGANZ <81500665+DIYONGANZ@users.noreply.github.com>
2021-03-30 14:23:25 +0700

    Add files via upload

---
+++ A187/help.js
@@ -0,0 +1,231 @@
+const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+        return `
+「 *${botName}* 」
+
+◪ *USER INFO*
+  ➥ Name: ${pushname}
+  ➥ XP: ${reqXp}
+  ➥ Money: ${uangku}
+  ➥ Registered: ✔️
+◪ *BOT INFO*
+  ➥ Prefix: 「  ${prefix}  」
+  ➥ Creator: ${ownerName}
+◪ *ABOUT*
+◪
+ ┃➥ ${prefix}info
+ ┃➥ ${prefix}blocklist
+ ┃➥ ${prefix}chatlist
+ ┃➥ ${prefix}ping
+ ┃➥ ${prefix}riquest
+ ┃➥ ${prefix}bugreport
+◪ *MENU*
+◪
+ ┃➥ ${prefix}logomakermenu
+ ┃➥ ${prefix}imagemakermenu
+ ┃➥ ${prefix}stickermakermenu
+ ┃➥ ${prefix}searchmenu
+ ┃➥ ${prefix}educationmenu
+ ┃➥ ${prefix}kerangmenu
+ ┃➥ ${prefix}downloadermenu
+ ┃➥ ${prefix}mememenu
+ ┃➥ ${prefix}groupmenu
+ ┃➥ ${prefix}soundmenu
+ ┃➥ ${prefix}musicmenu
+ ┃➥ ${prefix}islammenu
+ ┃➥ ${prefix}stalkmenu
+ ┃➥ ${prefix}wibumenu
+ ┃➥ ${prefix}18+menu
+ ┃➥ ${prefix}funmenu
+ ┃➥ ${prefix}todmenu
+ ┃➥ ${prefix}informationmenu
+ ┃➥ ${prefix}stayonscreenmenu
+ ┃➥ ${prefix}xpmenu
+ ┃➥ ${prefix}limitmenu
+ ┃➥ ${prefix}ownermenu
+ ┃➥ ${prefix}othermenu
+◪ *LOGO MAKER*
+◪
+ ┃➥ ${prefix}marvellogo
+ ┃➥ ${prefix}ninjalogo
+ ┃➥ ${prefix}logowolf
+ ┃➥ ${prefix}logowolf2
+ ┃➥ ${prefix}phlogo
+ ┃➥ ${prefix}neonlogo
+ ┃➥ ${prefix}neonlogo2
+ ┃➥ ${prefix}lionlogo
+ ┃➥ ${prefix}pubglogo
+ ┃➥ ${prefix}jokerlogo  
+◪ *DOWNLOADER*
+◪
+ ┃➥ ${prefix}pinterest
+ ┃➥ ${prefix}ytmp3
+ ┃➥ ${prefix}ytmp4
+ ┃➥ ${prefix}tiktok
+◪ *EDUCATION*
+◪
+ ┃➥ ${prefix}wiki
+ ┃➥ ${prefix}wikien
+ ┃➥ ${prefix}nulis
+ ┃➥ ${prefix}quotes
+ ┃➥ ${prefix}quotes2
+ ┃➥ ${prefix}artinama  
+◪ *FUN*
+◪
+ ┃➥ ${prefix}alay
+ ┃➥ ${prefix}gantengcek
+ ┃➥ ${prefix}watak
+ ┃➥ ${prefix}gay
+ ┃➥ ${prefix}trigger
+ ┃➥ ${prefix}wasted
+ ┃➥ ${prefix}hobby
+ ┃➥ ${prefix}game
+ ┃➥ ${prefix}bucin
+ ┃➥ ${prefix}trust
+ ┃➥ ${prefix}dare
+ ┃➥ ${prefix}simi  
+◪ *GROUP*
+◪
+ ┃➥ ${prefix}opengc
+ ┃➥ ${prefix}closegc
+ ┃➥ ${prefix}promote
+ ┃➥ ${prefix}demote
+ ┃➥ ${prefix}tagall
+ ┃➥ ${prefix}tagall2
+ ┃➥ ${prefix}tagall3
+ ┃➥ ${prefix}tagall4
+ ┃➥ ${prefix}tagall5
+ ┃➥ ${prefix}add
+ ┃➥ ${prefix}kick
+ ┃➥ ${prefix}listadmins
+ ┃➥ ${prefix}linkgroup
+ ┃➥ ${prefix}leave
+ ┃➥ ${prefix}welcome
+ ┃➥ ${prefix}nsfw
+ ┃➥ ${prefix}delete
+ ┃➥ ${prefix}simih
+ ┃➥ ${prefix}ownergroup  
+◪ *18+*
+ ┃➥ ${prefix}randomhentai
+ ┃➥ ${prefix}nsfwtrap
+ ┃➥ ${prefix}nsfwneko
+◪ *LIMIT*
+◪
+ ┃➥ ${prefix}limit
+ ┃➥ ${prefix}bal
+ ┃➥ ${prefix}buylimit  
+◪ *IMAGE MAKER*
+◪
+ ┃➥ ${prefix}bpink
+ ┃➥ ${prefix}snowwrite
+ ┃➥ ${prefix}3dtext
+ ┃➥ ${prefix}firetext
+ ┃➥ ${prefix}wasted
+ ┃➥ ${prefix}gay
+ ┃➥ ${prefix}glitch
+ ┃➥ ${prefix}shadow
+ ┃➥ ${prefix}burnpaper
+ ┃➥ ${prefix}coffee
+ ┃➥ ${prefix}lovepaper
+ ┃➥ ${prefix}woodblock
+ ┃➥ ${prefix}qowheart
+ ┃➥ ${prefix}mutgrass
+ ┃➥ ${prefix}undergocean
+ ┃➥ ${prefix}woodenboards
+ ┃➥ ${prefix}wolfmetal
+ ┃➥ ${prefix}metalictglow
+ ┃➥ ${prefix}8bit
+ ┃➥ ${prefix}herrypotter
+ ┃➥ ${prefix}quotemaker  
+◪ *INFORMATION*
+◪
+ ┃➥ ${prefix}bahasa
+ ┃➥ ${prefix}kodenegara
+ ┃➥ ${prefix}kbbi
+ ┃➥ ${prefix}fakta
+ ┃➥ ${prefix}infocuaca
+ ┃➥ ${prefix}infogempa
+ ┃➥ ${prefix}jadwaltvnow
+ ┃➥ ${prefix}covidcountry  
+◪ *ISLAM*
+◪
+ ┃➥ ${prefix}quran
+◪ *WIBU*
+◪
+ ┃➥ ${prefix}neonime
+ ┃➥ ${prefix}pokemon
+ ┃➥ ${prefix}loli
+ ┃➥ ${prefix}waifu
+ ┃➥ ${prefix}randomanime
+ ┃➥ ${prefix}husbu
+ ┃➥ ${prefix}husbu2
+ ┃➥ ${prefix}wait
+ ┃➥ ${prefix}nekonime
+◪ *TOD*
+◪
+ ┃➥ ${prefix}trust
+ ┃➥ ${prefix}dare
+◪ *STICKER MAKER*
+◪
+ ┃➥ ${prefix}sticker
+ ┃➥ ${prefix}stickergif
+ ┃➥ ${prefix}gay
+ ┃➥ ${prefix}wasted
+ ┃➥ ${prefix}trigger
+ ┃➥ ${prefix}ttp
+◪ *STAY ON SCREEN*
+◪
+ ┃➥ ${prefix}afk
+◪ *STALK*
+◪
+ ┃➥ ${prefix}tiktokstalk
+ ┃➥ ${prefix}igstalk
+◪ *SOUND*
+◪
+ ┃➥ ${prefix}tts
+◪ *MUSIC*
+◪
+ ┃➥ ${prefix}play
+ ┃➥ ${prefix}joox
+ ┃➥ ${prefix}lirik
+ ┃➥ ${prefix}chord
+◪ *MEME*
+◪
+ ┃➥ ${prefix}meme
+ ┃➥ ${prefix}memeindo  
+◪ *OTHER*
+◪
+ ┃➥ ${prefix}send
+ ┃➥ ${prefix}wame
+ ┃➥ ${prefix}virtex
+ ┃➥ ${prefix}qrcode
+ ┃➥ ${prefix}timer
+ ┃➥ ${prefix}fml
+ ┃➥ ${prefix}fml2
+◪ *OWNER*
+  
+ ┃➥ ${prefix}setprefix
+ ┃➥ ${prefix}block
+ ┃➥ ${prefix}unblock
+ ┃➥ ${prefix}bc
+ ┃➥ ${prefix}bcgc
+ ┃➥ ${prefix}ban
+ ┃➥ ${prefix}unban
+ ┃➥ ${prefix}clone
+ ┃➥ ${prefix}clearall  
+◪ *Search*
+◪
+ ┃➥ ${prefix}wait
+ ┃➥ ${prefix}ytsearch
+ ┃➥ ${prefix}trendtwit
+ ┃➥ ${prefix}wikien
+ ┃➥ ${prefix}wiki
+ ┃➥ ${prefix}neonime              
+◪ *TANYA*
+◪
+ ┃➥ ${prefix}apakah
+ ┃➥ ${prefix}kapankah
+ ┃➥ ${prefix}rate
+ ┃➥ ${prefix}bisakah`  
+  
+}
+exports.help = help
